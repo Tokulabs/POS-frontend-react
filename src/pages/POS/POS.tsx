8e2f00cedf4b2bb9e7c263cb9b36b927224731e7
oskarkos <os-da-ar@hotmail.com>
2024-04-05 23:36:34 -0500

    add right panel in POS

--- src/pages/POS/POS.tsx
+++ src/pages/POS/POS.tsx
@@ -1,18 +1,29 @@
+import { useEffect, useRef, useState } from 'react'
+// Third Party
 import { Select } from 'antd'
 import { useQueryClient } from '@tanstack/react-query'
-import { getInventoriesNew } from '../Inventories/helpers/services'
-import { useEffect, useRef, useState } from 'react'
-import { IInventoryProps } from '../Inventories/types/InventoryTypes'
 import { AxiosError } from 'axios'
-import { TableHeader } from './components/TableHeader'
-import { TableData } from './Data/TableData'
-import { TableRow } from './components/TableRow'
-import { IPosData } from './components/types/TableTypes'
-import { useCart } from '../../store/useCartStoreZustand'
 import { debounce } from 'lodash'
+import { IconTablePlus } from '@tabler/icons-react'
+// Service Helper
+import { getInventoriesNew } from '../Inventories/helpers/services'
+// Types
+import { IInventoryProps } from '../Inventories/types/InventoryTypes'
+import { IPosData } from './components/types/TableTypes'
+// Components
+import { TableHeader } from './components/TableHeader'
+import { TableRow } from './components/TableRow'
+// Data
+import { TableData } from './Data/TableData'
+// Store
+import { useCart } from '../../store/useCartStoreZustand'
+// Helpers
+import { formatNumberToColombianPesos, formatToUsd } from '../../utils/helpers'
 
 export const POS = () => {
   const [isLoadingSearch, setIsLoadingSearch] = useState(false)
+  const [data, setData] = useState<IInventoryProps[]>([])
+  const [value, setValue] = useState<string>()
 
   const queryClient = useQueryClient()
 
@@ -39,7 +50,7 @@ export const POS = () => {
       quantity: 1,
       total: data.selling_price,
       usd_total: data.usd_price,
-      photo: data.photo ?? '',
+      photo: data.photo || '',
     }
   }
 
@@ -59,9 +70,6 @@ export const POS = () => {
     }
   }
 
-  const [data, setData] = useState<IInventoryProps[]>([])
-  const [value, setValue] = useState<string>()
-
   const handleSearch = (newValue: string) => {
     debouncedSearch(newValue)
   }
@@ -71,7 +79,7 @@ export const POS = () => {
     const item = data.filter((item) => item.code === newValue)[0]
     addToCart(formatDatatoIPOSData(item))
     updateTotalPrice()
-    setValue('')
+    setValue(null as unknown as string)
   }
 
   const debouncedSearch = useRef(
@@ -87,8 +95,8 @@ export const POS = () => {
   }, [debouncedSearch])
 
   return (
-    <section className='w-full h-[calc(100vh-6.50rem)] flex'>
-      <section className='w-3/4 h-full p-5 bg-white'>
+    <section className='w-full h-[calc(100vh-6.50rem)] flex gap-6'>
+      <section className='w-3/4 h-full p-5 bg-white shadow-lg rounded-sm'>
         <header className='w-full'>
           <h1 className='text-2xl font-semibold'>Crear Venta</h1>
           <section className='w-full'>
@@ -124,28 +132,56 @@ export const POS = () => {
             </Select>
           </section>
         </header>
-        <TableHeader tableColumnsData={TableData} />
-        <ul className='p-0'>
-          {cartItems.length > 0 ? (
-            cartItems.map((product) => <TableRow key={product.code} product={product} />)
-          ) : (
-            <div className='w-full h-20 flex justify-center items-center font-medium text-2xl'>
-              <p>No hay productos ... </p>
-            </div>
-          )}
-        </ul>
+
+        {cartItems.length > 0 ? (
+          <section>
+            <TableHeader tableColumnsData={TableData} />
+            <ul className='p-0 divide-solid divide-y divide-gray-1'>
+              {cartItems.map((product) => (
+                <TableRow key={product.code} product={product} />
+              ))}
+            </ul>
+          </section>
+        ) : (
+          <div className='w-full h-96 flex justify-center flex-col items-center font-medium text-2xl text-gray-1'>
+            <IconTablePlus size={80} />
+            <p>Agregar productos </p>
+          </div>
+        )}
+
         <footer></footer>
       </section>
-      <nav className='w-1/4 h-full bg-green-1'>
-        <h1>Carrito</h1>
+      <nav className='w-1/4 h-full flex flex-col justify-between bg-white shadow-lg rounded-sm'>
+        <section>checkout</section>
         <section>
-          <h2>subtotal: {subtotalCOP}</h2>
-          <h2>subtotal USD: {subtotalUSD}</h2>
-          <h2>Descuento COP: {discountCOP}</h2>
-          <h2>Descuetno Dolares: {discountUSD}</h2>
-          <h2>IVA: {taxesIVACOP}</h2>
-          <h2>Total COP: {totalCOP}</h2>
-          <h2>Total USD: {totalUSD}</h2>
+          <div className='bg-gray-100 shadow-inner p-5 flex justify-center flex-col items-center gap-4 text-sm font-semibold'>
+            <div className='flex w-full justify-between items-center'>
+              <span>Subtotal COP</span>
+              <span>{formatNumberToColombianPesos(subtotalCOP)}</span>
+            </div>
+            <div className='flex w-full justify-between items-center'>
+              <span>Descuento</span>
+              <span className={discountCOP > 0 ? 'text-green-1' : ''}>
+                {formatNumberToColombianPesos(discountCOP)}
+              </span>
+            </div>
+            <div className='flex w-full justify-between items-center'>
+              <span>
+                IVA <span className='text-green-1'>19%</span>
+              </span>
+              <span>{formatNumberToColombianPesos(taxesIVACOP)}</span>
+            </div>
+          </div>
+          <div className='w-full border-solid border-t-[1px] border-x-0 border-b-0 border-green-1 rounded-b-sm p-5 flex flex-col gap-2 font-bold'>
+            <div className='flex justify-between items-end w-full'>
+              <span className='text-base'>Total a pagar COP</span>
+              <span className='text-xl text-green-1'>{formatNumberToColombianPesos(totalCOP)}</span>
+            </div>
+            <div className='flex justify-between items-end w-full'>
+              <span className='text-base'>Total a pagar USD</span>
+              <span className='text-xl text-green-1'>{formatToUsd(totalUSD)}</span>
+            </div>
+          </div>
         </section>
       </nav>
     </section>
