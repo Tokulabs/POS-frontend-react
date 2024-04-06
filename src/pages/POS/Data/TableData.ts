4139d655c2655955c97ba53687738fa0e2e2622a
oskarkos <os-da-ar@hotmail.com>
2024-04-05 23:37:13 -0500

    table header styles

--- src/pages/POS/Data/TableData.ts
+++ src/pages/POS/Data/TableData.ts
@@ -3,38 +3,38 @@ import { ITableTitles } from '../components/types/TableTypes'
 export const TableData: ITableTitles[] = [
   {
     tableTitle: 'Codigo',
-    tableStyles: 'text-start',
+    tableStyles: 'text-start w-full',
   },
   {
     tableTitle: 'Nombre',
-    tableStyles: 'col-span-2 text-left',
+    tableStyles: 'col-span-2 text-left w-full',
   },
   {
     tableTitle: 'Imagen',
-    tableStyles: 'col-start-4',
+    tableStyles: 'col-start-4 w-full',
   },
   {
     tableTitle: 'Precio COP',
-    tableStyles: 'col-start-5',
+    tableStyles: 'col-start-5 w-full',
   },
   {
     tableTitle: 'Precio USD',
-    tableStyles: 'col-start-6',
+    tableStyles: 'col-start-6 w-full',
   },
   {
     tableTitle: 'Descuento',
-    tableStyles: 'col-start-7',
+    tableStyles: 'col-start-7 w-full',
   },
   {
     tableTitle: 'Cantidad',
-    tableStyles: 'col-span-2 col-start-8',
+    tableStyles: 'col-span-2 col-start-8 w-full',
   },
   {
     tableTitle: 'Total USD',
-    tableStyles: 'col-start-10',
+    tableStyles: 'col-start-10 w-full',
   },
   {
     tableTitle: 'Total COP',
-    tableStyles: 'col-start-11 text-right',
+    tableStyles: 'col-start-11 text-right w-full',
   },
 ]
