917799689c7664b8c86bc7588f1199238501a868
oskarkos <os-da-ar@hotmail.com>
2024-04-05 23:37:43 -0500

    add middleware and refactor functions

--- src/store/useCartStoreZustand.ts
+++ src/store/useCartStoreZustand.ts
@@ -1,6 +1,7 @@
 import { create } from 'zustand'
 import { IPosData } from '../pages/POS/components/types/TableTypes'
-import { roundNumberToDecimals } from '../utils/helpers'
+import { calcMetaDataProdudct } from '../utils/helpers'
+import zustymiddlewarets from 'zustymiddlewarets'
 
 interface ICartStore {
   cartItems: IPosData[]
@@ -16,117 +17,140 @@ interface ICartStore {
   clearCart: () => void
   updateTotalPrice: () => void
   addDiscountToItem: (code: string, discount: number) => void
+  updateQuantity: (code: string, quantity: number) => void
 }
 
-export const useCart = create<ICartStore>((set, get) => ({
-  cartItems: [],
-  subtotalCOP: 0,
-  discountCOP: 0,
-  taxesIVACOP: 0,
-  subtotalUSD: 0,
-  discountUSD: 0,
-  totalCOP: 0,
-  totalUSD: 0,
-  addToCart: (product: IPosData) => {
-    const { cartItems, addDiscountToItem } = get()
-    const productExist = cartItems.find((item) => item.code === product.code)
-    if (productExist) {
-      const originalQuantity = productExist.quantity
-      productExist.quantity = originalQuantity + 1
-      productExist.total = productExist.quantity * product.selling_price
-      productExist.usd_total = productExist.quantity * product.usd_price
-      set({
-        cartItems: [...cartItems],
-      })
-      if (productExist.discount > 0) {
-        addDiscountToItem(product.code, productExist.discount * 100)
-      }
-    } else {
-      set({
-        cartItems: [...cartItems, product],
-      })
-    }
-  },
-  removeFromCart: (product: IPosData) => {
-    const { cartItems, addDiscountToItem } = get()
-    const productExist = cartItems.find((item) => item.code === product.code)
-    if (productExist) {
-      const originalQuantity = productExist.quantity
-      if (originalQuantity > 1) {
-        productExist.quantity = originalQuantity - 1
+export const useCart = create<ICartStore>(
+  zustymiddlewarets((set, get) => ({
+    cartItems: [],
+    subtotalCOP: 0,
+    discountCOP: 0,
+    taxesIVACOP: 0,
+    subtotalUSD: 0,
+    discountUSD: 0,
+    totalCOP: 0,
+    totalUSD: 0,
+    addToCart: (product: IPosData) => {
+      const { cartItems, addDiscountToItem } = get()
+      const productExist = cartItems.find((item) => item.code === product.code)
+      if (productExist) {
+        const originalQuantity = productExist.quantity
+        productExist.quantity = originalQuantity + 1
         productExist.total = productExist.quantity * product.selling_price
         productExist.usd_total = productExist.quantity * product.usd_price
         set({
           cartItems: [...cartItems],
         })
         if (productExist.discount > 0) {
-          addDiscountToItem(product.code, productExist.discount * 100)
+          addDiscountToItem(product.code, productExist.discount)
         }
       } else {
         set({
-          cartItems: cartItems.filter((item) => item.code !== product.code),
+          cartItems: [...cartItems, product],
         })
       }
-    }
-  },
-  clearCart: () => {},
-  updateTotalPrice: () => {
-    const { cartItems } = get()
-    let subtotalCOP = 0
-    let discountCOP = 0
-    let taxesIVACOP = 0
-    let subtotalUSD = 0
-    let discountUSD = 0
-    let totalCOP = 0
-    let totalUSD = 0
+    },
+    removeFromCart: (product: IPosData) => {
+      const { cartItems, addDiscountToItem } = get()
+      const productExist = cartItems.find((item) => item.code === product.code)
+      if (productExist) {
+        const originalQuantity = productExist.quantity
+        if (originalQuantity > 1) {
+          productExist.quantity = originalQuantity - 1
+          productExist.total = productExist.quantity * product.selling_price
+          productExist.usd_total = productExist.quantity * product.usd_price
+          set({
+            cartItems: [...cartItems],
+          })
+          if (productExist.discount > 0) {
+            addDiscountToItem(product.code, productExist.discount)
+          }
+        } else {
+          set({
+            cartItems: cartItems.filter((item) => item.code !== product.code),
+          })
+        }
+      }
+    },
+    clearCart: () => {},
+    updateTotalPrice: () => {
+      const { cartItems } = get()
+      let subtotalCOP = 0
+      let discountCOP = 0
+      let taxesIVACOP = 0
+      let subtotalUSD = 0
+      let discountUSD = 0
+      let totalCOP = 0
+      let totalUSD = 0
 
-    cartItems.forEach((item) => {
-      const priceQuantityCOP = item.quantity * item.selling_price
-      const priceQuantityUSD = item.quantity * item.usd_price
+      cartItems.forEach((item) => {
+        const {
+          itemDiscountCOP,
+          itemDiscountUSD,
+          itemTaxesCOP,
+          itemWithNoTaxCOP,
+          itemWithNoTaxUSD,
+          totalItemCOP,
+          totalItemUSD,
+        } = calcMetaDataProdudct(item)
 
-      const itemWithNoTaxCOP = priceQuantityCOP / 1.19
-      const itemWithNoTaxUSD = priceQuantityUSD / 1.19
-
-      const itemDiscountCOP = itemWithNoTaxCOP * item.discount
-      const itemDiscountUSD = itemWithNoTaxUSD * item.discount
-
-      const itemTaxesCOP = priceQuantityCOP - itemWithNoTaxCOP
-      const itemTaxesUSD = priceQuantityUSD - itemWithNoTaxUSD
-
-      const totalItemCOP = itemWithNoTaxCOP - itemDiscountCOP + itemTaxesCOP
-      const totalItemUSD = itemWithNoTaxUSD - itemDiscountUSD + itemTaxesUSD
-
-      subtotalCOP += Math.round(itemWithNoTaxCOP)
-      subtotalUSD += Math.round(itemWithNoTaxUSD)
-      discountCOP += Math.round(itemDiscountCOP)
-      taxesIVACOP += Math.round(itemTaxesCOP)
-      discountUSD += Math.round(itemDiscountUSD)
-      totalCOP += Math.round(totalItemCOP)
-      totalUSD += Math.round(totalItemUSD)
-    })
-    set({
-      subtotalCOP,
-      discountCOP,
-      taxesIVACOP,
-      subtotalUSD,
-      discountUSD,
-      totalCOP,
-      totalUSD,
-    })
-  },
-  addDiscountToItem: (code: string, discount: number) => {
-    const { cartItems } = get()
-    const productExist = cartItems.find((item) => item.code === code)
-    if (productExist) {
-      productExist.discount = discount / 100
-      const realTotal = productExist.quantity * productExist.selling_price
-      productExist.total = Math.round(realTotal - realTotal * productExist.discount)
-      const realUSDTotal = productExist.quantity * productExist.usd_price
-      const totalUSDValue = Math.round(realUSDTotal - realUSDTotal * productExist.discount)
-      productExist.usd_total = roundNumberToDecimals(totalUSDValue, 2)
-      set({
-        cartItems: [...cartItems],
+        subtotalCOP += Math.round(itemWithNoTaxCOP)
+        subtotalUSD += Math.round(itemWithNoTaxUSD)
+        discountCOP += Math.round(itemDiscountCOP)
+        taxesIVACOP += Math.round(itemTaxesCOP)
+        discountUSD += Math.round(itemDiscountUSD)
+        totalCOP += Math.round(totalItemCOP)
+        totalUSD += Math.round(totalItemUSD)
       })
-    }
-  },
-}))
+      set({
+        subtotalCOP,
+        discountCOP,
+        taxesIVACOP,
+        subtotalUSD,
+        discountUSD,
+        totalCOP,
+        totalUSD,
+      })
+    },
+    addDiscountToItem: (code: string, discount: number) => {
+      const { cartItems } = get()
+      const productExist = cartItems.find((item) => item.code === code)
+      if (productExist) {
+        productExist.discount = discount
+        set({
+          cartItems: [...cartItems],
+        })
+        const { totalItemCOP, totalItemUSD } = calcMetaDataProdudct(productExist)
+        productExist.total = totalItemCOP
+        productExist.usd_total = totalItemUSD
+        set({
+          cartItems: [...cartItems],
+        })
+      }
+    },
+    updateQuantity: (code: string, quantity: number) => {
+      const { cartItems } = get()
+      const productExist = cartItems.find((item) => item.code === code)
+      if (productExist) {
+        productExist.quantity = quantity
+        set({
+          cartItems: [...cartItems],
+        })
+        const { totalItemCOP, totalItemUSD } = calcMetaDataProdudct(productExist)
+        productExist.total = totalItemCOP
+        productExist.usd_total = totalItemUSD
+        set({
+          cartItems: [...cartItems],
+        })
+      }
+    },
+  })),
+)
+
+declare global {
+  interface Window {
+    store: typeof useCart
+  }
+}
+
+window.store = useCart
