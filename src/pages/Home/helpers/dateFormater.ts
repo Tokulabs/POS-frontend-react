80b8cfe3d02f4e03c70f88fc45f36b8317262229
oskarkos <os-da-ar@hotmail.com>
2024-05-26 22:47:36 -0500

    fix date week

--- src/pages/Home/helpers/dateFormater.ts
+++ src/pages/Home/helpers/dateFormater.ts
@@ -18,15 +18,11 @@ export const dateFormater = (date: string) => {
 }
 
 export const convertToCurrentWeek = (weekStr: string): string => {
-  const weekNumber = parseInt(weekStr.split(' ')[1], 10)
+  const weekNumber = parseInt(weekStr.split(' ')[1])
   const year = new Date().getFullYear()
-  const firstDayOfYear = new Date(year, 0, 1)
-  const firstDayWeekDay = firstDayOfYear.getDay()
-  const offsetToFirstMonday = firstDayWeekDay === 0 ? 1 : 8 - firstDayWeekDay
-  const dayOfYearOfWeekStart = (weekNumber - 1) * 7 + offsetToFirstMonday
-  const weekStart = new Date(year, 0, dayOfYearOfWeekStart)
-  const weekEnd = new Date(year, 0, dayOfYearOfWeekStart + 6)
-  const weekStartStr = `${weekStart.getDate()}/${weekStart.getMonth() + 1}`
-  const weekEndStr = `${weekEnd.getDate()}/${weekEnd.getMonth() + 1}`
-  return `${weekStartStr} - ${weekEndStr}`
+  const firstDay = new Date(year, 0, 1 + (weekNumber - 1) * 7)
+  const lastDay = new Date(year, 0, 1 + (weekNumber - 1) * 7 + 6)
+  const firstDayFormatted = `${firstDay.getDate()}/${firstDay.getMonth() + 1}`
+  const lastDayFormatted = `${lastDay.getDate()}/${lastDay.getMonth() + 1}`
+  return `${firstDayFormatted}-${lastDayFormatted}`
 }
