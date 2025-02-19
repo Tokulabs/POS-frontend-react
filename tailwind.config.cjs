f2a4912860dec3296bb8ea8895780bd5f2ec38d6
oskarkos <os-da-ar@hotmail.com>
2025-02-18 21:41:12 -0500

    update tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,16 +8,6 @@ module.exports = {
       gridTemplateColumns: {
         13: 'repeat(13, minmax(0, 1fr))',
       },
-
-      // keyframes: { // remove it
-      // 	'caret-blink': {
-      // 	  '0%,70%,100%': { opacity: '1' },
-      // 	  '20%,50%': { opacity: '0' },
-      // 	},
-      // },
-      // animation: { // remove it
-      // 	'caret-blink': 'caret-blink 1.25s ease-out infinite',
-      // },
       colors: {
         green: {
           1: '#269962',
@@ -26,54 +16,6 @@ module.exports = {
           1: '#b2bec3',
           2: '#757575',
         },
-        // 	background: 'hsl(var(--background))', remove it
-        // 	red: {
-        // 		'1': '#E62C37'
-        // 	},
-        // 	foreground: 'hsl(var(--foreground))',
-        // 	card: {
-        // 		DEFAULT: 'hsl(var(--card))',
-        // 		foreground: 'hsl(var(--card-foreground))'
-        // 	},
-        // 	popover: {
-        // 		DEFAULT: 'hsl(var(--popover))',
-        // 		foreground: 'hsl(var(--popover-foreground))'
-        // 	},
-        // 	primary: {
-        // 		DEFAULT: 'hsl(var(--primary))',
-        // 		foreground: 'hsl(var(--primary-foreground))'
-        // 	},
-        // 	secondary: {
-        // 		DEFAULT: 'hsl(var(--secondary))',
-        // 		foreground: 'hsl(var(--secondary-foreground))'
-        // 	},
-        // 	muted: {
-        // 		DEFAULT: 'hsl(var(--muted))',
-        // 		foreground: 'hsl(var(--muted-foreground))'
-        // 	},
-        // 	accent: {
-        // 		DEFAULT: 'hsl(var(--accent))',
-        // 		foreground: 'hsl(var(--accent-foreground))'
-        // 	},
-        // 	destructive: {
-        // 		DEFAULT: 'hsl(var(--destructive))',
-        // 		foreground: 'hsl(var(--destructive-foreground))'
-        // 	},
-        // 	border: 'hsl(var(--border))',
-        // 	input: 'hsl(var(--input))',
-        // 	ring: 'hsl(var(--ring))',
-        // 	chart: {
-        // 		'1': 'hsl(var(--chart-1))',
-        // 		'2': 'hsl(var(--chart-2))',
-        // 		'3': 'hsl(var(--chart-3))',
-        // 		'4': 'hsl(var(--chart-4))',
-        // 		'5': 'hsl(var(--chart-5))'
-        // 	}
-        // },
-        // borderRadius: {
-        // 	lg: 'var(--radius)',
-        // 	md: 'calc(var(--radius) - 2px)',
-        // 	sm: 'calc(var(--radius) - 4px)'
       },
       borderRadius: {
         lg: 'var(--radius)',
